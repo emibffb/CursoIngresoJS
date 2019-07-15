bc9b7d191c17910e0583101584fd6f1fab816cbc
emibffb <52058263+emibffb@users.noreply.github.com>
2019-07-15 10:07:55 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,22 @@ function mostrar()
 {
 //tomo la edad  
 
+    //var edad;
+    //edad = document.getElementById("edad").value;
+    //if (edad >=13 && edad <17 ){
+        //alert("es adolecente");
+    //}
 
+    var edad;
+    edad = document.getElementById("edad").value;
+    if(edad >= 13)
+    {
+    
+        if (edad <= 17)
+        {
+            alert("es adolescente")
+        }
+            
+    }
 
 }//FIN DE LA FUNCIÓN
