3b6387621b79003ea069e574a60d81625e18c599
emibffb <52058263+emibffb@users.noreply.github.com>
2019-08-03 10:12:02 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,24 @@
-function mostrar()
-{
+function mostrar() {
+    var nota;
+    var sexo;
+    var promedioNotas;
+    var NotaBaja;
+    var SexoyNota;
+    var notaVaron;
+    var contador = 0
+
+    while (contador < 5) {
+        nota = parseInt(prompt("Ingrese una nota"));
+        while(nota <0 || nota > 10){
+            nota = prompt ("Error ingrese una nota entre 0 y 10");
+        }
+            sexo = prompt("Ingrese un sexo")
+       while(sexo !='f' || sexo !='m'){
+           sexo = prompt("Error ingrese un sexo válido");
+       }
+
+        
+    }
+
 
 }
